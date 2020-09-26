5c219a47a0e5fa843f1a3d89d6c16c176040e3ec
takahiro ootani <taka.oot@gmail.com>
2020-09-26 20:12:55 +0900

    RSSI読み取り対応

--- ESP32/ESP32_BLE/ESP32_BLE.ino
+++ ESP32/ESP32_BLE/ESP32_BLE.ino
@@ -21,8 +21,8 @@ Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 BLEScan* pBLEScan;
 
 String near1[30]; //mac
-int  near2[30];  //rssi
-int near_count=0;
+uint8_t  near2[30];  //rssi
+int near_count = 0;
 void bdaDump(esp_bd_addr_t bd) {
   for (int i = 0; i < ESP_BD_ADDR_LEN; i++) {
     Serial.printf("%02x", bd[i]);
@@ -62,8 +62,8 @@ class dataCb: public BLECharacteristicCallbacks {
       Serial.println("load");
       //// 送りたいデータ(とりあえず3つ)
       //// TODO:ここにセンサデータを代入する
-      uint16_t DATA1 = mlx.readObjectTempC()*100;//体温
-      uint16_t DATA2 = mlx.readAmbientTempC()*100;//気温
+      uint16_t DATA1 = mlx.readObjectTempC() * 100; //体温
+      uint16_t DATA2 = mlx.readAmbientTempC() * 100; //気温
       uint16_t DATA3 = 12;
       ///
       memset(buf, 0, sizeof buf);               // バッファーを0クリア
@@ -82,26 +82,39 @@ class dataCb2: public BLECharacteristicCallbacks {
     void onRead(BLECharacteristic *pChar) {
       uint8_t buf[7*30];
       Serial.println("load2");
-      for(int i = 0; i < 30;i++) {
+      for (int i = 0; i < 30; i++) {
         int offset = i * 7;
-       String mac = near1[i];
+        String mac = near1[i];
         mac.replace(":", "");
-       byte macByte[4];
+         String mac2 = mac.substring(0, 5+1);
+         mac = mac.substring(6, 11+1);
+        Serial.println("1:"+mac);
+         Serial.println("2:"+mac2);
+             Serial.println(near2[i]);
+        byte macByte[3];
         unsigned long number = strtoul( mac.c_str(), nullptr, 16);
+         Serial.println(number);
+        for (int j = 3-1; j >= 0; j--) // start with lowest byte of number
+        {
+          macByte[j] = number & 0xFF;  // or: = byte( number);
+          number >>= 8;            // get next byte into position
+        }
+      byte macByte2[3];
+     number = strtoul( mac2.c_str(), nullptr, 16);
+         Serial.println(number);
+        for (int j = 3-1; j >= 0; j--) // start with lowest byte of number
+        {
+          macByte2[j] = number & 0xFF;  // or: = byte( number);
+          number >>= 8;            // get next byte into position
+        }
 
-  for(int j=6; j>=0; j--)    // start with lowest byte of number
-  {
-    macByte[j] = number & 0xFF;  // or: = byte( number);
-    number >>= 8;            // get next byte into position
-  }
-  
-      buf[offset+0] = (uint8_t)(macByte[0]);
-        buf[offset+1] = (uint8_t)(macByte[1]);
-          buf[offset+2] = (uint8_t)(macByte[2]);
-            buf[offset+3] = (uint8_t)(macByte[3]);
-              buf[offset+4] = (uint8_t)(macByte[4]);
-                buf[offset+5] = (uint8_t)(macByte[5]);
-                   buf[offset+6] = (uint8_t)(near2[i]*-1);
+        buf[offset + 0] = (uint8_t)(macByte2[0]);
+        buf[offset + 1] = (uint8_t)(macByte2[1]);
+        buf[offset + 2] = (uint8_t)(macByte2[2]);
+        buf[offset + 3] = (uint8_t)(macByte[0]);
+        buf[offset + 4] = (uint8_t)(macByte[1]);
+          buf[offset + 5] = (uint8_t)(macByte[2]);
+        buf[offset + 6] = (uint8_t)(near2[i]);
       }
       pChar->setValue(buf, sizeof buf);         // データを書き込み
     }
@@ -112,19 +125,19 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
       //TODO これらのデータをどこか変数において、要求があったときにデバイスアドレスとRSSIを送信できるようにする
       String info = advertisedDevice.toString().c_str();
       int idx = info.indexOf("Rssi: ");
-       int idx2 = info.indexOf("Address: ")+9;
-      if(idx == -1) {
-      Serial.printf("No RSSi");   
-      }else{
-       Serial.println(idx);
-      String mac  =  info.substring(idx2,idx2+17);
-      info  =info.substring(idx)+5;
-      Serial.printf("Advertised Device: %s ; RSSI=%s\n", advertisedDevice.toString().c_str(),info.c_str());
-      if(near_count <=30) {
-      near1[near_count]=mac;
-       near2[near_count]=atoi(info.c_str())*-1;
-      near_count++;
-      }
+      int idx2 = info.indexOf("Address: ") + 9;
+      if (idx == -1) {
+        Serial.printf("No RSSi");
+      } else {
+        String mac  =  info.substring(idx2, idx2 + 17);
+        info  = info.substring(idx+1+6);
+        //Serial.printf("Advertised Device: %s ; RSSI=%s\n", advertisedDevice.toString().c_str(), info.c_str());
+        if (near_count <= 30) {
+          near1[near_count] = mac;
+                Serial.println(info.c_str());
+          near2[near_count] = (uint8_t)(atoi(info.c_str()));
+          near_count++;
+        }
       }
     }
 };
@@ -137,11 +150,11 @@ void setup() {
   Serial.begin(115200);
   Serial.println("Starting BLE work!");
 
-  for(int i = 0; i < 30;i++) {
-  near1[i] = "00:00:00:00:00:00"; //mac
-near2[i] = 0;  //rssi
+  for (int i = 0; i < 30; i++) {
+    near1[i] = "00:00:00:00:00:00"; //mac
+    near2[i] = 0;  //rssi
   }
-  
+
   BLEDevice::init("ESP32");                  // デバイスを初期化
   BLEServer *pServer = BLEDevice::createServer();    // サーバーを生成
   pServer->setCallbacks(new MyServerCallbacks());    // コールバック関数を設定
@@ -154,12 +167,12 @@ near2[i] = 0;  //rssi
   pService->start();                                 // サービスを起動
 
   //TODO　もう1つサービスを作る　デバイスアドレスとRSSIを送るためのサービス
-BLEService *pService2 = pServer->createService(SENSOR_UUID2); 
+  BLEService *pService2 = pServer->createService(SENSOR_UUID2);
   pService2->createCharacteristic(LATESTDATA_UUID2, BLECharacteristic::PROPERTY_READ)
-  ->setCallbacks(new dataCb());                  // コールバック関数を設定
+  ->setCallbacks(new dataCb2());                  // コールバック関数を設定
 
-  pService2->start();    
-  
+  pService2->start();
+
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->addServiceUUID(SENSOR_UUID);
   pAdvertising->setScanResponse(true);
@@ -176,16 +189,16 @@ BLEService *pService2 = pServer->createService(SENSOR_UUID2);
   pBLEScan->setWindow(99);  // less or equal setInterval value
 
   //温度センサ
-  Serial.println("Adafruit MLX90614 test"); 
+  Serial.println("Adafruit MLX90614 test");
   mlx.begin();
 }
 void loop() {
   delay(5000);
-  for(int i = 0; i < 30;i++) {
-  near1[i] = "00:00:00:00:00:00"; //mac
-near2[i] = 0;  //rssi
+  for (int i = 0; i < 30; i++) {
+    near1[i] = "00:00:00:00:00:00"; //mac
+    near2[i] = 0;  //rssi
   }
-  near_count=0;
+  near_count = 0;
   BLEScanResults foundDevices = pBLEScan->start(3, false); //3秒スキャン
   Serial.print("Devices found: ");
   Serial.println(foundDevices.getCount());
--- Ras-Pi/ble.py
+++ Ras-Pi/ble.py
@@ -14,105 +14,132 @@ headers = {
 }
 #####
 
+def temp_send(mac,taion, situon):
+	global headers
+	url = "http://api.bee3.tokyo/temp/register"
+	payloads = {
+		"mac" : mac,
+		"temp1" : taion,
+		"temp2" : situon
+	}
+	r = requests.get(url, headers=headers, params=payloads)
+	return r.status_code
+
+
 connected_list = {}
 rec_time = {}
 needClose=False
 uuid="b0c8c0fa-6d46-11e8-adc0-fa7ae01bbebc" #必要に応じて変更する
+uuid2="b1c8c0fa-6d46-11e8-adc0-fa7ae01bbebc" #必要に応じて変更する
 def worker(dev,worker_id):
-    global connected_list
-    global uuid
-    try:
-        print( "worker%s: connecting" % (worker_id) )
-        peri = bluepy.btle.Peripheral()
-        peri.connect(dev,"public")
-        print( "worker%s: connected" % (worker_id) )
-        #peri.withDelegate(MyDelegate(bluepy.btle.DefaultDelegate,worker_id))
-        while True:
-            if needClose == True:
-                break
-            latestDataRow = peri.getCharacteristics(uuid=uuid)[0]
+	global connected_list
+	global uuid
+    global uuid2
+	try:
+		print( "worker%s: connecting" % (worker_id) )
+		peri = bluepy.btle.Peripheral()
+		peri.connect(dev,"public")
+		print( "worker%s: connected" % (worker_id) )
+		#peri.withDelegate(MyDelegate(bluepy.btle.DefaultDelegate,worker_id))
+		while True:
+			if needClose == True:
+				break
+			latestDataRow = peri.getCharacteristics(uuid=uuid)[0]
+			dataRow = latestDataRow.read()
+			print("worker%s: data=%s" % (worker_id,dataRow))
+			(seq,DATA1, DATA2, DATA3) = struct.unpack('<Bhhh', dataRow) #受信データのデコード
+			print("worker%s: [SEQ%s]decoded DATA1=%s DATA2=%s DATA3=%s" % (worker_id,seq,DATA1, DATA2, DATA3))
+			temp_send(mac=dev.addr ,taion=DATA2, situon=DATA1)
+			#TODO: ESP32から見た他のESP32の検出結果を取得する
+            latestDataRow = peri.getCharacteristics(uuid=uuid2)[0]
             dataRow = latestDataRow.read()
-            print("worker%s: data=%s" % (worker_id,dataRow))
-            (seq,DATA1, DATA2, DATA3) = struct.unpack('<Bhhh', dataRow) #受信データのデコード
-            print("worker%s: [SEQ%s]decoded DATA1=%s DATA2=%s DATA3=%s" % (worker_id,seq,DATA1, DATA2, DATA3))
-            #TODO: ESP32から見た他のESP32の検出結果を取得する
-            time.sleep(1) #1秒ごとにデータ取得要求
-        print( "worker%s: disconnected(Close needed)" % (worker_id) )
-        peri.disconnect()
-    except Exception as e:
-        print("worker%s error:%s"%(worker_id,e))
-        print( "worker%s: disconnected" % (worker_id) )
-        #connected_list.pop(dev.addr)
+            #print("worker%s: data2=%s" % (worker_id,dataRow))
+            for i in range(30):
+                offset = i*7
+                macadr = format(dataRow[offset+0], 'x').zfill(2)+":"+format(dataRow[offset+1], 'x').zfill(2)+":"+format(dataRow[offset+2], 'x').zfill(2)+":"+format(dataRow[offset+3], 'x').zfill(2)+":"+format(dataRow[offset+4], 'x').zfill(2)+":"+format(dataRow[offset+5], 'x').zfill(2)
+                if macadr=="00:00:00:00:00:00" :
+                    break
+                print("worker%s: %s RSSI=-%s" % (worker_id,macadr,dataRow[offset+6]))
+			time.sleep(1) #1秒ごとにデータ取得要求
+		print( "worker%s: disconnected(Close needed)" % (worker_id) )
+		peri.disconnect()
+	except Exception as e:
+		print("worker%s error:%s"%(worker_id,e))
+		print( "worker%s: disconnected" % (worker_id) )
+		#connected_list.pop(dev.addr)
 
 def main():
-    global devadr
-    global connected_list
-    global needClose
-    counter=0
-    scanner = bluepy.btle.Scanner()
-    while True :
-        #if devadr != '' :
-        #    time.sleep(1)
-        #    continue
-        try:
-            print("search ESP32")
-            needClose=True
-            time.sleep(2)
-            devices = scanner.scan(3.0)  #1秒スキャン
-            needClose=False
-            connected_list_local = {}
-            #TODO: 登録デバイスの取得
-            for device in devices:
-                for (adtype, desc, value) in device.getScanData():
-                    if device.addrType =="public":
-                        print('======================================================')
-                        print('address : %s' % device.addr)
-                        print('addrType: %s' % device.addrType)
-                        print('RSSI    : %s' % device.rssi)
-                        print('desc    : %s' % desc)
-                        print('value    : %s' % value)
-                        if device.addr in connected_list_local.keys() : #接続済みの場合はスキップ
-                            print("(connected)")
-                            continue
-                        connected_list_local[device.addr]=1
-                        print("[%s] found" % value)
-                        if device.addr not in connected_list.keys() :
-                            print("API processing...") #入館した
-                            payloads = {
-                            "mac" : device.addr,
-                            "time": int(datetime.now().timestamp()),
-                            "status": "enter"
-                            }
-                            r = requests.get(url_inout, headers=headers, params=payloads)
-                            print("API processed RESULT=%s" % (r.status_code))
-                            connected_list[device.addr]=1
-                        #TODO: RSSI強度(or距離)の送信
-                        t = threading.Thread(target=worker, args=(device,counter))
-                        t.start()
-                        print("connected ==>%s" %(counter))
-                        time.sleep(0.4) #次の接続処理まで1秒待機
-                        counter+=1
-            print("Scan end")
-            #退館チェック
-            for d in list(connected_list.keys())[:]:
-                if d not in connected_list_local.keys() :
-                    print("API processing...") #退館した
-                    payloads = {
-			    "mac" : d,
-			    #"time": int(datetime.now().timestamp()), timeパラメータを省略するとサーバー側に到達した時間が登録される
-			    "status": "exit"
-                    }
-                    r = requests.get(url_inout, headers=headers, params=payloads)
-                    print("API processed RESULT=%s" % (r.status_code))
-                    connected_list.pop(d)
+	global devadr
+	global connected_list
+	global needClose
+	counter=0
+	scanner = bluepy.btle.Scanner()
+	while True :
+		#if devadr != '' :
+		#    time.sleep(1)
+		#    continue
+		try:
+			print("search ESP32")
+			needClose=True
+			time.sleep(1.5)
+			devices = scanner.scan(1.0)  #1秒スキャン
+			needClose=False
+			connected_list_local = {}
+			#TODO: 登録デバイスの取得
+			for device in devices:
+				for (adtype, desc, value) in device.getScanData():
+					if device.addrType =="public":
+						print('======================================================')
+						print('address : %s' % device.addr)
+						print('addrType: %s' % device.addrType)
+						print('RSSI    : %s' % device.rssi)
+						print('desc    : %s' % desc)
+						print('value    : %s' % value)
+						if device.addr in connected_list_local.keys() : #接続済みの場合はスキップ
+							print("(connected)")
+							continue
+						connected_list_local[device.addr]=1
+						print("[%s] found" % value)
+						if device.addr not in connected_list.keys() :
+							print("API processing...") #入館した
+							payloads = {
+							"mac" : device.addr,
+							#"time": int(datetime.now().timestamp()),
+							"status": "enter",
+							"floor": 1
+							}
+							r = requests.get(url_inout, headers=headers, params=payloads)
+							print("API processed RESULT=%s" % (r.status_code))
+							connected_list[device.addr]=1
+						#TODO: RSSI強度(or距離)の送信
+						t = threading.Thread(target=worker, args=(device,counter))
+						t.start()
+						print("connected ==>%s" %(counter))
+						time.sleep(0.4) #次の接続処理まで1秒待機
+						counter+=1
+			print("Scan end")
+			#退館チェック
+			for d in list(connected_list.keys())[:]:
+				if d not in connected_list_local.keys() :
+					print("API processing...") #退館した
+					payloads = {
+				"mac" : d,
+				#"time": int(datetime.now().timestamp()), timeパラメータを省略するとサーバー側に到達した時間が登録される
+				"status": "exit",
+				"floor": 1
+					}
+					r = requests.get(url_inout, headers=headers, params=payloads)
+					print("API processed RESULT=%s" % (r.status_code))
+					connected_list.pop(d)
 
-        except Exception as e:
-            print(e)
-            scan=False
-            print( "scanner error")
-            time.sleep(5)
-        time.sleep(30) #スキャン間隔
+		except Exception as e:
+			print(e)
+			scan=False
+			print( "scanner error")
+			time.sleep(5)
+        if len(connected_list) >0 :
+            time.sleep(30) #スキャン間隔
 
 
 if __name__ == "__main__":
-    main()
+	main()
